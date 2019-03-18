edcfa2ee48e963470779a35795725de3c2af01bf
PatrickHickey <codewithpat@gmail.com>
2019-03-18 00:24:29 -0700

    Added js and css to client

---
+++ client/src/components/images.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+export class images extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default images;
